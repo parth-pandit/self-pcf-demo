d24b584892cb2c1116fb727200248f89db371161
Parth Pandit <ppandit@pivotal.io>
2018-05-02 16:21:50 -0500

    Initial commit

---
+++ src/main/java/com/pgp/pcfdemo/PcfDemoApplication.java
@@ -0,0 +1,30 @@
+package com.pgp.pcfdemo;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PcfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfDemoApplication.class, args);
+	}
+}
+
+@RestController
+class PcfDemoController {
+	@RequestMapping("/greetings")
+    public String greet() {
+ 	    return "Hello, Adwait!!";
+    }
+}
+
+@Data
+@Accessors (fluent = true)
+class Greeting {
+    private String message;
+}
